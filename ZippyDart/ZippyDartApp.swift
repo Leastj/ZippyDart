a3932da1c933b96e07418a62e178320ed8cfa366
Léa Sautjeau <lsautjeau@gmail.com>
2024-01-22 20:01:24 +0100

    Initial Commit

---
+++ ZippyDart/ZippyDartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZippyDartApp.swift
+//  ZippyDart
+//
+//  Created by SAUTJEAU Lea on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZippyDartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
