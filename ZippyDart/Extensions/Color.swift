57b2c3d875180ef06c374731982c1feb84b1ebea
Léa Sautjeau <lsautjeau@gmail.com>
2024-02-01 12:32:12 +0100

    Second project commit

---
+++ ZippyDart/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  ZippyDart
+//
+//  Created by SAUTJEAU Lea on 23/01/2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let softGray = Color("SoftGray")
+    let whiteZippy = Color("WhiteZippy")
+    let slateGray = Color("SlateGray")
+    let lockGray = Color("LockGray")
+    let dimGray = Color("DimGray")
+    let cadetBlue = Color("CadetBlue")
+    let redZippy = Color("RedZippy")
+    let darkGray = Color("DarkGray")
+    
+}
