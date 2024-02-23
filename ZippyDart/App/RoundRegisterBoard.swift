9fd251355e2f0bc2a406f202ca3f0ac8e5683041
Léa Sautjeau <lsautjeau@gmail.com>
2024-02-23 15:12:17 +0100

    RoundRegisterBoard front terminé

---
+++ ZippyDart/App/RoundRegisterBoard.swift
@@ -0,0 +1,18 @@
+//
+//  RoundRegisterBoard.swift
+//  ZippyDart
+//
+//  Created by SAUTJEAU Lea on 06/02/2024.
+//
+
+import SwiftUI
+
+struct RoundRegisterBoard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundRegisterBoard()
+}
